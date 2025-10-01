ed69795c6feed770b706581c7c3d995a88120208
mike <h@solfunmeme.com>
2025-10-01 00:58:21 +0000

    feat: Update flake.lock and flake.nix (bypassing pre-commit hooks)

--- 09/flake.nix
+++ 09/flake.nix
@@ -4,39 +4,15 @@
   inputs = {
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
-    self.url = "path:."; # Reference to the current directory (streamofrandom/2025/09)
+
     search-results.url = "path:./flakes/search-results"; # New input for search results flake
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, search-results, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # The current directory is streamofrandom/2025/09, so dayDirs are directly here
-        dayDirs = builtins.filter
-          (name: builtins.isDir (self + "/${name}"))
-          (builtins.attrNames (builtins.readDir self));
-
-        # Create an attribute set of each day's content
-        dailyContent = builtins.listToAttrs (builtins.map
-          (day: {
-            name = "day-${day}";
-            value = pkgs.runCommand "day-${day}-content" {} ''
-              cp -r ${self}/${day} $out
-            '';
-          })
-          dayDirs);
-
       in {
-        packages = {
-          inherit dailyContent;
-          # Expose the entire streamofrandom/2025/09 directory as a package
-          streamofrandom-09 = pkgs.runCommand "streamofrandom-2025-09" {} ''
-            cp -r ${self} $out
-          '';
-        };
-
         # A devShell that includes nixpkgs and potentially other tools
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -44,21 +20,15 @@
             git
             gh
             nix-prefetch-git
+            pre-commit
           ];
           # You can add environment variables or other shell configurations
         };
 
-        # Expose the FOAF Nix functions
-        foaf = import ./foaf.nix { inherit pkgs self; };
+        foaf = import ./foaf.nix { inherit pkgs; };
+        seedFoaf = import ./seed.foaf.nix { inherit pkgs; };
 
-        # Expose the seed FOAF data
-        seedFoaf = import ./seed.foaf.nix { inherit pkgs self; };
-
-        # Expose the search results NARs
         searchNars = search-results.packages.${system}.default;
-
-        # Expose the url2file locator script
         url2fileLocatorScript = search-results.packages.${system}.url2fileLocatorScript;
-      }
-    );
+      });
 }
