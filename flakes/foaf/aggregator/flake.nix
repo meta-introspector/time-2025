6b67a1d7f7ad5c95e143fae411f86ec27c31a864
mike <h@solfunmeme.com>
2025-10-02 22:22:03 +0000

    CRQ-041(fix): Remove flake-utils.lib.eachDefaultSystem from flakes/foaf/aggregator/flake.nix to isolate syntax error.

--- flakes/foaf/aggregator/flake.nix
+++ flakes/foaf/aggregator/flake.nix
@@ -14,10 +14,8 @@
 
   outputs = { self, nixpkgs, flake-utils, foafContextFlake, foafSeedDataFlake,
               foafAggregatorContextFlake, foafAggregatorSeedGraphFlake, foafAggregatorFullGraphFlake }:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        # Temporarily return a simple set to debug syntax error
-        foo = "bar";
-      }
-    );
+    {
+      # Temporarily return a simple set to debug syntax error
+      foo = "bar";
+    };
 }
