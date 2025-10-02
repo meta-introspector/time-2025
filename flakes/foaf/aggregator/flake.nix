e58a491b90da99af3a30140f7b7614efaf24753a
mike <h@solfunmeme.com>
2025-10-02 22:11:18 +0000

    CRQ-041(feat): Modify flakes/foaf/aggregator/flake.nix to be a meta-aggregator.

--- flakes/foaf/aggregator/flake.nix
+++ flakes/foaf/aggregator/flake.nix
@@ -6,11 +6,24 @@
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
     foafContextFlake.url = "./../context";
     foafSeedDataFlake.url = "./../seed-data";
+    foafAggregatorContextFlake.url = "./context";
+    foafAggregatorSeedGraphFlake.url = "./seed-graph";
+    foafAggregatorFullGraphFlake.url = "./full-graph";
   };
 
-  outputs = { self, nixpkgs, flake-utils, foafContextFlake, foafSeedDataFlake }:
-    {
-      # Temporarily return a simple set to debug syntax error
-      foo = "bar";
-    };
+  outputs = { self, nixpkgs, flake-utils, foafContextFlake, foafSeedDataFlake,
+              foafAggregatorContextFlake, foafAggregatorSeedGraphFlake, foafAggregatorFullGraphFlake }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+      in
+      {
+        lib = {
+          inherit (foafAggregatorContextFlake.lib) foafContext;
+          inherit (foafAggregatorSeedGraphFlake.lib) seedGraph;
+          inherit (foafAggregatorFullGraphFlake.lib) fullGraph;
+        };
+      }
+    );
 }
