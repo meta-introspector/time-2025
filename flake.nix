fb81280577c88254cad10a885c8c5275760b8699
mike <h@solfunmeme.com>
2025-10-02 21:23:25 +0000

    CRQ-041(feat): Expose foafContextFlake's lib in the root flake's outputs.

--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
     foafContextFlake.url = "./flakes/foaf/context";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, foafContextFlake, ... }:
     let
       system = "aarch64-linux"; # Explicitly define system for debugging
       pkgs = nixpkgs.legacyPackages.${system};
       streamofrandom09Outputs = (import ./09/flake.nix).outputs { inherit nixpkgs flake-utils self; };
     in {
       devShell = streamofrandom09Outputs.${system}.devShells.default;
+      lib = {
+        inherit (foafContextFlake.lib) foafContext;
+      };
     };
 }
