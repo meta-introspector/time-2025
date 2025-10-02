d08e2d686d94fa35954040d7a597b38098ae5164
mike <h@solfunmeme.com>
2025-10-02 22:06:01 +0000

    CRQ-041(fix): Simplify flakes/foaf/aggregator/flake.nix to isolate syntax error.

--- flakes/foaf/aggregator/flake.nix
+++ flakes/foaf/aggregator/flake.nix
@@ -9,23 +9,8 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, foafContextFlake, foafSeedDataFlake }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib;
-
-        # Get the FOAF context from the context flake
-        foafContext = foafContextFlake.lib.foafContext;
-        # Get the seed graph from the seed data flake
-        seedGraph = foafSeedDataFlake.lib.seedGraph;
-
-        # Combine them into a full graph (initially just seed data with context)
-        fullGraph = { "@context" = foafContext; "@graph" = seedGraph; };
-      in
-      {
-        lib = {
-          inherit foafContext seedGraph fullGraph;
-        };
-      }
-    )
-};;;;;;;;;;;;;;;;;;;;
+    {
+      # Temporarily return a simple set to debug syntax error
+      foo = "bar";
+    };
+}
