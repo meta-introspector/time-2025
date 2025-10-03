5998e17fb6b943b43ae5679c1ad4c52d2578400b
mike <h@solfunmeme.com>
2025-10-03 01:26:16 +0000

    CRQ-042(fix): Remove flake-utils.lib.eachDefaultSystem and hardcode system in flakes/foaf/aggregator/flake.nix.

--- flakes/foaf/aggregator/flake.nix
+++ flakes/foaf/aggregator/flake.nix
@@ -3,14 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
-    flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
     foafContextFlake.url = "./../context";
     foafSeedDataFlake.url = "./../seed-data";
   };
 
-  outputs = { self, nixpkgs, flake-utils, foafContextFlake, foafSeedDataFlake }:
+  outputs = { self, nixpkgs, foafContextFlake, foafSeedDataFlake }:
+    let
+      system = "aarch64-linux"; # Hardcoded system
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
+
+      # Get the FOAF context from the context flake
+      foafContext = foafContextFlake.lib.foafContext;
+      # Get the seed graph from the seed data flake
+      seedGraph = foafSeedDataFlake.lib.seedGraph;
+
+      # Combine them into a full graph (initially just seed data with context)
+      fullGraph = { "@context" = foafContext; "@graph" = seedGraph; };
+    in
     {
-      # Temporarily return a simple set to debug syntax error
-      foo = "bar";
+      lib = {
+        inherit foafContext seedGraph fullGraph;
+      };
     };
 }
