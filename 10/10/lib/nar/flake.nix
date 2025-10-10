d670d37ce328808bfc0a8e8c6c7d49a61c11d843
mike <h@solfunmeme.com>
2025-10-10 20:06:41 +0000

    wip

--- 10/10/lib/nar/flake.nix
+++ 10/10/lib/nar/flake.nix
@@ -3,15 +3,15 @@
   description = "A flake to export the nix-file-list to a NAR file";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    get-nix-file-list-input.url = "github:meta-introspector/time-2025?ref=feature/lattice-30030-homedir&dir=10/10/nix2/get-nix-file-list.nix";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, get-nix-file-list-input }:
     let
       system = "aarch64-linux";
       pkgs = import nixpkgs { inherit system; };
-      nix2-src = pkgs.lib.sources.cleanSource ./nix2;
-      get-nix-file-list = nix2-src + "/get-nix-file-list.nix";
+      get-nix-file-list = get-nix-file-list-input;
     in
     {
       packages.${system}.default = pkgs.runCommand "nix-file-list-nar" {} ''
