de08c74ea3cb60904c72d71ec03973097d95f342
mike <h@solfunmeme.com>
2025-10-14 15:09:50 +0000

    Fix: Correctly access run_flake_sh_test_env in test_flake.nix

--- 10/12/audit-flakes/001_collect_locks/test_flake.nix
+++ 10/12/audit-flakes/001_collect_locks/test_flake.nix
@@ -21,10 +21,19 @@
         checks.testFlakeSh = pkgs.runCommand "test-flake-sh"
           {
             nativeBuildInputs = [ pkgs.bash ];
-            testScript = collect_locks_flake.test_flake_sh; # Access as a top-level attribute
+
+            # Define test parameters
+            NIX_FILE_PATH = "/data/data/com.termux.nix/files/home/pick-up-nix2/source/github/meta-introspector/streamofrandom/2025/flake.nix";
+            lockFile = "/data/data/com.termux.nix/files/home/pick-up-nix2/source/github/meta-introspector/streamofrandom/2025/flake.lock";
+            BAG_OF_WORDS_GENERATOR_PATH = "/data/data/com.termux.nix/files/home/pick-up-nix2/source/github/meta-introspector/streamofrandom/2025/flakes/bag-of-words-generator";
+
+            runTestScript = collect_locks_flake.outputs.${system}.run_flake_sh_test_env; # Path to the test runner script
           } ''
-          # Execute the test script
-          bash $testScript
+          # Execute the test runner script with parameters as environment variables
+          export NIX_FILE_PATH="$NIX_FILE_PATH"
+          export lockFile="$lockFile"
+          export BAG_OF_WORDS_GENERATOR_PATH="$BAG_OF_WORDS_GENERATOR_PATH"
+          bash $runTestScript
         '';
       });
 }
